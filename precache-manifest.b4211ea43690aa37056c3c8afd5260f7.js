54a41cc5f512e85b742a1a461f1aa859ac242911
Alex Wade <alex.wade13@gmail.com>
2020-03-22 21:59:03 -0400

    Updates

---
+++ precache-manifest.b4211ea43690aa37056c3c8afd5260f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa9b375d5008490c821f",
+    "url": "/algolia/static/js/runtime~main.fa9b375d.js"
+  },
+  {
+    "revision": "a011f3eef89eecbed473",
+    "url": "/algolia/static/js/main.a011f3ee.chunk.js"
+  },
+  {
+    "revision": "4ebd8d8c59f162820729",
+    "url": "/algolia/static/js/1.4ebd8d8c.chunk.js"
+  },
+  {
+    "revision": "a011f3eef89eecbed473",
+    "url": "/algolia/static/css/main.74a03d1d.chunk.css"
+  },
+  {
+    "revision": "b7d4ab5f87aeb8b3961cf3c6c8d4a416",
+    "url": "/algolia/index.html"
+  }
+];
